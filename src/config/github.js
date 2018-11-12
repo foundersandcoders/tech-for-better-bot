a3484667edad2184c3861cad5096425456d3f144
Michael Watts <hello@michaelwatts.co>
2018-11-12 11:17:14 +0000

    use console.error to show error in heroku logs

--- src/config/github.js
+++ src/config/github.js
@@ -1,5 +1,6 @@
 const token = process.env.GITHUB_TOKEN ? process.env.GITHUB_TOKEN : false;
 if (token == false) {
+  console.error('Github token must be set in environment variables');
   throw new Error('Github token must be set in environment variables');
 }
 

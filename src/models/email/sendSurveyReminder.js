b19284b9931a05ec3061e2077ae6e12de3ef7fd6
Oliver <oliversw.artist@gmail.com>
2019-10-14 17:03:08 +0100

    Update sendSurveyReminder.js

--- src/models/email/sendSurveyReminder.js
+++ src/models/email/sendSurveyReminder.js
@@ -10,14 +10,15 @@ const sendSurveyReminder = record => {
   const subject = "Tech for Better user research reminder";
   const html = `
   <p>Hi there,</p>
-  <p>Just a gentle reminder that the deadline for returning the results of your user research is ${userResearchDeadline
+  <p>Just a gentle reminder that the deadline for applying to the programme is ${userResearchDeadline
     .split("-")
-    .join(" ")}. In order for your project to be up for selection by the London cohort, we'll need the results back before that date.</p>
+    .join(" ")}. In order for your project to be up for selection by the London cohort, we'll need your application, 
+including the results of your user research before that date.</p>
   <p>Here's the <a href="${researchSurveyUrl}?prefill_Email=${
     record.fields["Email"]
   }&prefill_application_id=${record.id}&prefill_Name=${
     record.fields["Name"]
-  }">form</a>, in case you need it again.</p>
+  }">application form</a>, in case you need it again.</p>
   <p>As always, don't hesitate to ask me any questions you might have!</p>
   <p>Many thanks,</p>
   <p><b>${name}</b>
